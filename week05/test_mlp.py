a9997bd02c7def9d9a06d2e7b04a84d6da28f057
cuongtv <cuongtv@vnu.edu.vn>
2021-10-09 18:43:34 +0700

    Add week05

---
+++ week05/test_mlp.py
@@ -0,0 +1,76 @@
+"""
+Name:
+Class:
+MSSV:
+
+You should understand your code
+
+Several works can be added:
+- Improve accuracy
+- Improve speed: parrallel processing
+- Add more parameters
+"""
+
+import pandas as pd
+import numpy as np
+import math
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
+
+
+def add_noise_data(input_data, input_labels, n_points, mean, scale):
+    """
+    Create a noise verstion of the input data
+
+    Params:
+        input_data: base input data
+        input_labels: base input labels
+        n_points: the number of needed points
+        mean, scale: the gaussian data
+    """
+    raw_X = []
+    raw_labels = []
+
+    noise = np.random.normal(loc=mean, scale=scale, size=(n_points, 2))
+    for i in range(n_points):
+        k = np.random.randint(len(input_data))
+
+        x1 = input_data[k][0] + noise[i][0]
+        x2 = input_data[k][1] + noise[i][1]
+
+        # We add more difficult for decision tree
+
+        raw_X.append([x1 + x2, x1*x2, x1*x1,
+                      math.sin(math.sqrt(abs(x1))), 1/(1 + math.exp(-x2)), x1/(abs(x2) + 1e-5),
+                      math.sin(x1 + x2)])
+
+        raw_labels.append(input_labels[k])
+
+    return np.array(raw_X), np.array(raw_labels)
+
+
+
+if __name__ == "__main__":
+    np.random.seed(1)
+
+    n_train = 10000
+    std_train = 0.1
+
+    n_test = 1000
+    std_test = 0.5
+
+    and_X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    and_y = np.array([0, 1, 1, 0])
+
+    Xtrain, ytrain = add_noise_data(and_X, and_y, n_train, 0., std_train)
+    print(Xtrain.shape, ytrain.shape)
+
+    mlp = SimpleMLP()
+    model = mlp.fit(Xtrain, ytrain)
+
+    Xtest, ytest = add_noise_data(and_X, and_y, n_test, 0., std_test)
+
+    output_train = mlp.predict(Xtrain)
+    output_test = mlp.predict(Xtest)
+    print("Accuracy on train: ", accuracy_score(ytrain, output_train))
+    print("Accuracy: ", accuracy_score(ytest, output_test))
